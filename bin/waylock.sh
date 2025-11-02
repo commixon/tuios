eb83835f7122447da0dba0eb3fa2d9649691519a
commixon <commixon@proton.me>
2025-11-02 14:49:11 +0100

    Fix waylock

--- bin/waylock.sh
+++ bin/waylock.sh
@@ -4,7 +4,8 @@ source ~/.config/tuios/theme/colors.sh
 
 timestamp=$(date +%F-%R:%S)
 
-chayang || exit 1
+# chayang || exit 1
 
-export WAYLAND_DEBUG=1
-waylock -init-color "$RIVER_COLOR1" -input color "$RIVER_COLOR4" --fork-on-lock >/tmp/waylock-${timestamp}.log 2>&1
+# export WAYLAND_DEBUG=1
+# waylock -init-color "$RIVER_COLOR1" -input color "$RIVER_COLOR4" --fork-on-lock >/tmp/waylock-${timestamp}.log 2>&1
+waylock -init-color "$RIVER_COLOR1" -input-color "$RIVER_COLOR4"
